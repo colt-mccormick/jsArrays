6c0a80c09e7dffbc16dfe33d17f2f34cb0187e71
Colt McCormick <coltmccormick@hotmail.com>
2018-02-27 19:46:58 -0600

    completed challenge 1

--- arrays.js
+++ arrays.js
@@ -1,1 +1,17 @@
 console.log("Hello World!")
+
+//exercise 1
+
+var test1 = 'cat'; 
+var test2 = 'racecar';
+var test3 = 'bear';
+var test4= 'mom'; 
+
+var opp = test1.split("").reverse().join(""); 
+
+if(test1 === opp){
+    console.log("same: ", true); 
+}else {
+    console.log("same: ", false);
+}
+
