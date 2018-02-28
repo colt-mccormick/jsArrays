61a4e68070a95e6b4689c862a17b2b517e4ee0d3
Colt McCormick <coltmccormick@hotmail.com>
2018-02-27 21:18:26 -0600

    completed challenge 2

--- arrays.js
+++ arrays.js
@@ -15,3 +15,27 @@ if(test1 === opp){
     console.log("same: ", false);
 }
 
+// class example
+
+
+var animals = ['fish', 'dog', 'cat'];
+var animalzElement = document.getElementById('animalz')
+for(var i=0; i<animals.length; i++) {
+    //console.log("animals: ", animals[i])
+    animalzElement.innerHTML += "<h5>" + animals[i] + "</h5>";
+}
+
+//exercise #2
+
+var book1 = "Catch 22"; 
+var book2 = "Slaughterhouse 5"; 
+var book3 = "Fahrenheit 451"; 
+
+var bookSplit= book3.split(""); 
+var realNumz = []; 
+for(var j=0; j<bookSplit.length; j++){
+    if(bookSplit[j]*)) {
+        realNumz.push(bookSplit[j]); 
+    }
+}
+console.log("answer", realNumz.join(""));
